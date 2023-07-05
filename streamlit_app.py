60b2d7bbd68d14753926c7cd0f717270be11a217
HamidInetum <138670644+HamidInetum@users.noreply.github.com>
2023-07-05 14:20:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
-streamlit.title('Working with Fred: Breakfast in Algiers')
+streamlit.title('Working with Fred: Breakfast in Algiers- 🐔 🐔 🐔')
 streamlit.text('Milk and dates')
 streamlit.text('oats and bananas')
