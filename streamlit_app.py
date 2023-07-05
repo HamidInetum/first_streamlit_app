21aee3f05119cf501bea0b8250881ed214e351d2
HamidInetum <138670644+HamidInetum@users.noreply.github.com>
2023-07-05 14:25:00 +0200

    Update streamlit_app.py
    Reading CSV file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
+import pandas pd
+
 streamlit.title('Working with Fred: Breakfast in Algiers- 🐔 🐔 🐔')
 streamlit.text('Milk and dates')
 streamlit.text('oats and bananas')
+
+# reading CSV files
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
