e88a8c5a84a8d993bb7f88122f63dde3f11b4798
HamidInetum <138670644+HamidInetum@users.noreply.github.com>
2023-07-05 14:30:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ streamlit.text('oats and bananas')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # adding multiselect feature
-streamlit.multiselect("Pick your fruits buddy !",list(my_fruits_list.index))
+streamlit.multiselect("Pick your fruits buddy !",list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
