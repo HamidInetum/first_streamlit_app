bc4035b6d0da40b5c2c85cf8278b6181281aa195
HamidInetum <138670644+HamidInetum@users.noreply.github.com>
2023-07-06 11:39:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,11 @@ fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/"+fruit
 
 fruityvice_norm = pandas.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_norm);
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
