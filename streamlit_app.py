b76564b9fd96d372f7e662d0b1d04834d4377c96
HamidInetum <138670644+HamidInetum@users.noreply.github.com>
2023-07-06 10:04:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 
 streamlit.title('Working with Fred: Breakfast in Algiers- 🐔 🐔 🐔')
 streamlit.text('Milk and dates')
-streamlit.text('oats and bananas')
+streamlit.text('oats, bananas and peanut butter')
 
 # reading CSV files
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
