90c691ea44126436aeab29aef97eb211ff60dd9d
HamidInetum <138670644+HamidInetum@users.noreply.github.com>
2023-07-06 10:02:43 +0200

    Update streamlit_app.py
    Adding request calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('Working with Fred: Breakfast in Algiers- 🐔 🐔 🐔')
 streamlit.text('Milk and dates')
@@ -13,3 +14,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 selected_fruits = streamlit.multiselect("Pick your fruits buddy !",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[selected_fruits]
 streamlit.dataframe(fruits_to_show)
+
+# New Section to display fruityvice API response
+
+fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response);
