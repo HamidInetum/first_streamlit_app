6412bd5861929601ff34c5d18e1bb9365a0a3d06
HamidInetum <138670644+HamidInetum@users.noreply.github.com>
2023-07-06 10:14:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json());
+# streamlit.text(fruityvice_response.json());
 
 fruityvice_norm = pandas.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_norm);
